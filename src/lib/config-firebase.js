29d33f892f0b01431460c965eb57271506200058
Jessika Mirella <jessikamirellabraga@gmail.com>
2022-06-03 16:33:12 -0300

    firebase-config

---
+++ src/lib/config-firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCERzq8TukvV2hZ5kTrDTNAjZVuFfxfwmk",
+    authDomain: "lab-notes-c2bd4.firebaseapp.com",
+    projectId: "lab-notes-c2bd4",
+    storageBucket: "lab-notes-c2bd4.appspot.com",
+    messagingSenderId: "605360616142",
+    appId: "1:605360616142:web:fb1f40c15fa820a348569f"
+};
+
+const app = initializeApp(firebaseConfig);
