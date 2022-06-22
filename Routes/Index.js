2cc1b2b8b0f7e66a3e614ab50197161c7a775480
Jessika Mirella <jessikamirellabraga@gmail.com>
2022-06-21 21:46:09 -0300

    Updates

--- Routes/Index.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { Navigate, Outlet } from "react-router-dom";
-import { AuthGoogleContext } from "../lib/authentication";
-
-export const PrivateRoutes = () => {
-    const { singned } = useContext(AuthGoogleContext);
-    return singned ? <Outlet /> : <Navigate to="/Login" />;
-};
